c50b1aeade53e5388ababd737ad2318680fcf797
Sherest Mehta <sherestdutt@gmail.com>
2024-09-11 11:38:20 +0530

    Patch linked pictures

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -64,6 +64,7 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
+  decorateLinkedPictures(main);
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
@@ -131,3 +132,17 @@ async function loadPage() {
 }
 
 loadPage();
+
+/**
+ * Decorates linked pictures in a given block.
+ * @param {HTMLElement} block - The block element containing the pictures.
+ */
+function decorateLinkedPictures(block) {
+  block.querySelectorAll('picture + br + a').forEach((a) => {
+    // remove br
+    a.previousElementSibling.remove();
+    const picture = a.previousElementSibling;
+    a.textContent = '';
+    a.append(picture);
+  });
+}
